6f97ce516de32af5287923b8427f939f207053f6
DevPatel1023 <devp4143@gmail.com>
2025-08-01 21:58:49 +0530

    bug fix

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,21 +1,25 @@
 import { Hono } from "hono";
 import { signupUser } from "../services/userService";
-export const userRoute = new Hono();
 
-userRoute.post('/signup',async (c)=>{
-    try {
-        const { email , name , password } = await c.req.json()
-        const user = await signupUser(email,name,password)
-        return c.json({
-            message : 'new user created',
-            user
-        })
-    } catch (e : any) {
-        return c.json({
-            error : e.message
-        },400)
-    }
-});
-userRoute.post('/signin',(c)=>{
-    return c.text('signin')
-});
+export const userRoute = new Hono<{
+  Bindings: { DATABASE_URL: string }
+}>()
+
+userRoute.post('/signup', async (c) => {
+  try {
+    const { email, name, password } = await c.req.json()
+    const user = await signupUser(c.env.DATABASE_URL, email, name, password)
+    return c.json({
+      message: 'new user created',
+      user
+    })
+  } catch (e: any) {
+    return c.json({
+      error: e.message
+    }, 400)
+  }
+})
+
+userRoute.post('/signin', (c) => {
+  return c.text('signin')
+})
--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -1,15 +1,26 @@
 import { PrismaClient } from '../generated/prisma'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-// create prisma client with accelerate
-const prisma = new PrismaClient().$extends(withAccelerate());
 
-export async function signupUser(email : string , name : string ,password : string){
-    const isExisitingUser = await prisma.user.findUnique({ where : { email } })
-    if(isExisitingUser){
-        throw new Error('user already exists');
-    } 
-    return prisma.user.create({
-        data : { email , name , password }
-    })
+export async function signupUser(
+  dbUrl: string,
+  email: string,
+  name: string,
+  password: string
+) {
+  const prisma = new PrismaClient({
+    datasourceUrl: dbUrl
+  }).$extends(withAccelerate())
+
+  const isExisitingUser = await prisma.user.findUnique({
+    where: { email }
+  })
+
+  if (isExisitingUser) {
+    throw new Error('user already exists')
+  }
+
+  return prisma.user.create({
+    data: { email, name, password }
+  })
 }
