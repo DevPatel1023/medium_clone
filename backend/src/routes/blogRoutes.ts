b46f4219fbcc5e629b2fcc84df52c8b9ca58c92d
DevPatel1023 <devp4143@gmail.com>
2025-08-03 16:57:30 +0530

    adding blog routes and services

--- backend/src/routes/blogRoutes.ts
+++ backend/src/routes/blogRoutes.ts
@@ -1,12 +1,88 @@
 import { Hono } from "hono";
-export const blogRoute = new Hono();
+import { createBlog, getBlogId, updateBlog } from "../services/blogService";
+import { env } from "hono/adapter";
 
-blogRoute.post('/',(c)=>{
-    return c.text('blog post')
+export const blogRoute = new Hono<{
+    Bindings : {
+        DATABASE_URL : string,
+    }
+}>();
+
+// create blog
+blogRoute.post('/create',async (c) =>{
+    try {
+        const {title,content,authorId} = await c.req.json();
+
+        const {newBlog} = await createBlog(
+            c.env.DATABASE_URL,
+            title,
+            content,
+            authorId
+        )
+        return c.json(
+            newBlog
+        );
+    } catch (error) {
+        return c.json({
+            error
+        },400);
+    }
 });
-blogRoute.put('/',(c)=>{
-    return c.text('update blog')
+
+// update blog
+blogRoute.put('/:id',async(c)=>{
+    try {
+        const {id} = await c.req.param();
+        const {title,content} = await c.req.json();
+
+        const {updatedBlog} = await updateBlog(
+            c.env.DATABASE_URL,
+            id,
+            title,
+            content,
+        )
+        return c.json(
+            updateBlog
+        );
+    } catch (error) {
+        return c.json({
+            error
+        },400);
+    }
 });
-blogRoute.get('/:id',(c)=>{
-    return c.text('signin')
+
+// get blog by id
+blogRoute.get('/:id',async(c)=>{
+    try {
+        const {id} = await c.req.param();
+
+        const {blog} = await getBlogId(
+            c.env.DATABASE_URL,
+            id,
+        )
+        return c.json(
+            blog
+        );
+    } catch (error) {
+        return c.json({
+            error
+        },400);
+    }
+});
+
+// get all blogs 
+blogRoute.get('/blogs',async(c)=>{
+    try {
+
+        const {blogs} = await getBlogs(
+            c.env.DATABASE_URL,
+        )
+        return c.json(
+            blog
+        );
+    } catch (error) {
+        return c.json({
+            error
+        },400);
+    }
 });
---
+++ backend/src/services/blogService.ts
@@ -0,0 +1,89 @@
+import { PrismaClient } from "../generated/prisma/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+export async function createBlog(
+  db_url: string,
+  title: string,
+  content: string,
+  authorId: string
+) {
+  const prisma = new PrismaClient({
+    datasourceUrl: db_url,
+  }).$extends(withAccelerate());
+
+  // create new blog and store in db
+  const newBlog =  await prisma.blog.create({
+    data : {title , content ,authorId}
+  });
+
+  return {
+    newBlog
+  };
+}
+
+export async function updateBlog(
+  db_url: string,
+  id : string,
+  title: string,
+  content: string,
+) {
+  const prisma = new PrismaClient({
+    datasourceUrl: db_url,
+  }).$extends(withAccelerate());
+
+  // update blog 
+  const updatedBlog = await prisma.blog.update({
+    where : { id },
+    data : {title , content}
+  });
+
+  return {
+    updatedBlog
+  }
+}
+
+export async function getBlogId(
+  db_url: string,
+  id : string,
+) {
+  const prisma = new PrismaClient({
+    datasourceUrl: db_url,
+  }).$extends(withAccelerate());
+
+  // get blog by id
+  const blog =  await prisma.blog.findUnique({
+    where : { id },
+  });
+
+  return {
+    blog
+  }
+}
+
+export async function getBlogs(
+  db_url: string,
+  page : number = 1 
+) {
+  const prisma = new PrismaClient({
+    datasourceUrl: db_url,
+  }).$extends(withAccelerate());
+
+  const limit = 10;
+  const skip = (page - 1) * limit;
+
+  // get blog by id
+  const blogs = await prisma.blog.findMany({
+    skip,
+    take : limit,
+    orderBy: {
+      createdAt: 'desc',
+    },
+    author : true
+  });
+
+  const total = await prisma.blog.count();
+
+  return {
+    blogs
+  };
+}
