ebf649f48ec9f5ea7515a20899428c0d30077ae9
DevPatel1023 <devp4143@gmail.com>
2025-08-04 20:59:50 +0530

    added zod validation in backend for user input

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Hono } from "hono";
 import { signinUser, signupUser } from "../services/userService";
 import { env } from "hono/adapter";
+import { signInInput, signupInput } from "@adp_2sdcp/common-medium-clone";
 
 export const userRoute = new Hono<{
-  Bindings: { 
+  Bindings: {
     DATABASE_URL: string;
     JWT_SECRET: string;
   };
@@ -18,6 +19,12 @@ userRoute.post("/signup", async (c) => {
     //   username : string,
     //   password : string,
     // }
+
+    const result = signupInput.safeParse({ email, password, username: name });
+    if (!result.success) {
+      return c.json({ error: result.error.format() }, 411);
+    }
+
     const { JWT_SECRET } = env<{ JWT_SECRET: string }>(c);
 
     const { newUser, jwt_token } = await signupUser(
@@ -44,10 +51,15 @@ userRoute.post("/signup", async (c) => {
 
 userRoute.post("/signin", async (c) => {
   try {
-    const { email , password } = await c.req.json();
-    const {JWT_SECRET} = env<{JWT_SECRET : string}>(c);
+    const { email, password } = await c.req.json();
+
+    const result = signupInput.safeParse({ email, password });
+    if (!result.success) {
+      return c.json({ error: result.error.format() }, 411);
+    }
+    const { JWT_SECRET } = env<{ JWT_SECRET: string }>(c);
     // pass the data to the fun
-    const {user,jwt_token} = await signinUser(
+    const { user, jwt_token } = await signinUser(
       c.env.DATABASE_URL,
       JWT_SECRET,
       email,
@@ -55,13 +67,13 @@ userRoute.post("/signin", async (c) => {
     );
 
     return c.json({
-      message : 'welcome user',
+      message: "welcome user",
       user,
-      jwt_token
-    })
-  } catch (e : any) {
-     return c.json({
-      error : e.message
-     });
+      jwt_token,
+    });
+  } catch (e: any) {
+    return c.json({
+      error: e.message,
+    });
   }
 });
