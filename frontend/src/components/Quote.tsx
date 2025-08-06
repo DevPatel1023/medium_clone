cab2b2aa584d8e6226e99aacb258f82cf6dde15f
DevPatel1023 <devp4143@gmail.com>
2025-08-06 10:22:22 +0530

    pages and routing setup

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,9 @@
+const Quote = () => {
+  return (
+    <div className="bg-slate-200">
+      <div className="flex justify-center items-center bg-red-300">hi</div>
+    </div>
+  );
+};
+
+export default Quote;
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,14 @@
+import Quote from "../components/Quote"
+
+const Signup = () => {
+  return (
+    <div className="grid md:grid-cols-2 h-screen">
+      <div className="flex justify-center items-center">
+      signup
+      </div>
+      <Quote />
+    </div>
+  )
+}
+
+export default Signup
